647494b146a3433e6dffd14911ad8c873ff82e60
mijikim <miji.kim926@gmail.com>
2014-06-17 14:04:45 -0600

    Finished minimum stories for fruits.rb

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,27 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+puts fruits.length
+
+puts fruits.index('bananas')
+
+print fruits.insert(1, 'rasberries')
+
+puts ''
+
+fruits.each do |f|
+
+  puts f.length
+
+end
+
+puts ''
+
+fruits.each do |f|
+
+  puts f.upcase
+
+end
+
+puts
+
+puts fruits.select{|f| f.include? 'g'}
