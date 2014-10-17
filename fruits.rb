d174d654171b1e047b2cb04c15357228c2e4cc7d
mijikim <miji.kim926@gmail.com>
2014-10-17 00:24:49 -0600

    print length of the fruits array

--- fruits.rb
+++ fruits.rb
@@ -1,27 +1,47 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
 
+#print length of the array
 puts fruits.length
 
-puts fruits.index('bananas')
+#pring index of bananas
 
-print fruits.insert(1, 'rasberries')
 
-puts ''
 
-fruits.each do |f|
 
-  puts f.length
 
-end
 
-puts ''
 
-fruits.each do |f|
 
-  puts f.upcase
 
-end
 
-puts
-
-puts fruits.select{|f| f.include? 'g'}
+# puts fruits.length
+#
+# puts fruits.index('bananas')
+#
+# print fruits.insert(1, 'rasberries')
+#
+# puts ''
+#
+# fruits.each do |f|
+#
+#   puts f.length
+#
+# end
+#
+# puts ''
+#
+# fruits.each do |f|
+#
+#   puts f.upcase
+#
+# end
+#
+# puts
+#
+# puts fruits.select{|f| f.include? 'g'}
+#
+# fruits.each do |f|
+#
+#   puts f if f.include? 'g'
+#
+# end
